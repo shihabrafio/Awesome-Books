8aaf01cae7d0bf9c20c33f879cb48fed12eba488
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-04-23 20:37:18 +0600

    asd

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = document.querySelector(".button");
+
+const books = [
+  {
+    title: "Faust",
+    author: "Goethe",
+  },
+  {
+    title: "For Whom the Bell Tolls",
+    author: "Ernest Hemingway",
+  },
+  {
+    title: "Old Man and the Sea",
+    author: "Ernest Hemingway",
+  },
+  {
+    title: "Freedom at Midnight",
+    author: "Collins",
+  },
+  {
+    title: "Jungle Book",
+    author: "Ruskin Bond",
+  },
+  {
+    title: "Illiad",
+    author: "Homer",
+  },
+];
