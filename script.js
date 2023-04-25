e1991fa4018edb1647ec72d3b3d858392f29cb5e
Yordi <yorditjordin@gmail.com>
2023-04-25 13:24:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,84 @@
+const list = document.querySelector('#list');
 const add = document.querySelector('.button');
 const remove = document.querySelector('.close');
-const books = JSON.parse(localStorage.getItem('new-list')) || [
+let books = JSON.parse(localStorage.getItem('new-list')) || [
   {
     title: 'Faust',
     author: 'Goethe',
+    id: 0,
   },
   {
     title: 'For Whom the Bell Tolls',
     author: 'Ernest Hemingway',
+    id: 1,
   },
   {
     title: 'Old Man and the Sea',
     author: 'Ernest Hemingway',
+    id: 2,
   },
-  {
-    title: 'Freedom at Midnight',
-    author: 'Collins',
-  },
-  {
-    title: 'Jungle Book',
-    author: 'Ruskin Bond',
-  },
-  {
-    title: 'Illiad',
-    author: 'Homer',
-  },
+  // {
+  //   title: 'Freedom at Midnight',
+  //   author: 'Collins',
+  // },
+  // {
+  //   title: 'Jungle Book',
+  //   author: 'Ruskin Bond',
+  // },
+  // {
+  //   title: 'Illiad',
+  //   author: 'Homer',
+  // },
 ];
 
 // When some1 hits add
 
-function addFunction(e) {
-  e.preventDefault();
-  const title = document.querySelector('.title');
-  const author = document.querySelector('.author');
-  const list = document.querySelector('#list');
-
-  const newbook = {
-    title: title.value,
-    author: author.value,
-  };
-
-  books.push(newbook);
-  /// ///////// display all books
-  /// / first reset list
-  // put it to locall server
-
+function renderBooks(){
   localStorage.setItem('new-list', JSON.stringify(books));
   list.innerHTML = '';
-
-  // then loop through books array and add + everything
   books.forEach((book, index) => {
     list.innerHTML += ` 
   <div id="boi${index}" data-index=${index}>
   <p> ${book.title} <br /> ${book.author} </p>
   <p>
-    <button class="close" id=${index} data-index=${index} onclick='removeFunction(this)'>Remove</button>
+    <button class="close" id=${index} onclick='removeFunction(this)'>Remove</button>
   </p>
   <hr />
   </div>
  `;
   });
 }
-add.addEventListener('click', addFunction);
-function removeFunction(button) {
-  const num = button.id;
-  const rmv = document.getElementById(`boi${num}`);
-  rmv.remove();
-  books.splice(num);
-  localStorage.setItem('new-list', JSON.stringify(books));
+
+function addFunction(e) {
+  e.preventDefault();
+  const title = document.querySelector('.title');
+  const author = document.querySelector('.author');
+  if(title.value.trim() === '' || author.value.trim() === ''){
+    return;
+  }
+  const newbook = {
+    title: title.value,
+    author: author.value,
+    id: books.length
+  };
+  books.push(newbook);
+  document.querySelector('form').reset();
+  renderBooks();
 }
+
+function updateIndex(){
+  books.forEach((book, index)=>{ 
+    book.id = index;
+  })
+}
+
+function removeFunction(button) {
+  const num = parseInt(button.id); 
+  books = books.filter((book) => book.id !== num )
+  updateIndex();
+  renderBooks();
+}
+
+add.addEventListener('click', addFunction);
+
+window.location.reload = renderBooks();
